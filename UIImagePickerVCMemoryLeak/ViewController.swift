1f492044da59566d2f3d14f42200765ac0b331d5
Arda Dinler <adinler@ford.com.tr>
2022-04-27 08:34:50 +0300

    add imagepicker from gallery for test memory leak

--- UIImagePickerVCMemoryLeak/ViewController.swift
+++ UIImagePickerVCMemoryLeak/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
+    @IBAction func openGallery(_ sender: Any) {
+        pickImage(fromSource: .photoLibrary)
+    }
+
+    func pickImage(fromSource sourceType: UIImagePickerController.SourceType) {
+
+        guard UIImagePickerController.isSourceTypeAvailable(sourceType) else { return }
+
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = sourceType
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+
+        if let image = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
+//            handle image
+            print("handle image")
+        } else if let image = info[UIImagePickerController.InfoKey.editedImage.rawValue] as? UIImage {
+//            handle image
+            print("handle image2")
+        }
+
+        picker.dismiss(animated: true)
+    }
 }
 
